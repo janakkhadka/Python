c3a32a4c27f19a7f50184673ce01e3f5fbee7665
janakkhadka <janakkhadka08@gmail.com>
2024-06-11 23:13:11 +0545

    day1 final

--- Day1/hello.py
+++ Day1/hello.py
@@ -1,3 +1,18 @@
 print("hello")
-
 1+1
+print("doesn't")
+
+c = 3 * 'un' + 'ium'
+print(c)
+
+d = 'janak' + 'khadka'
+print(d)
+
+word = 'Kathmanu'
+word[5]
+word[-1]
+word[2:5]
+word[:2]+word[3:]
+
+
+
