0fc132c157827b85b02a99b56d1525adb4eb3e42
janakkhadka <janakkhadka08@gmail.com>
2024-06-13 17:37:39 +0545

    learning control flow on day 3

---
+++ Day3/controlflow.py
@@ -0,0 +1,18 @@
+#if elif
+x = input("Enter the value of x: ")
+x = float(x)  
+
+if x < 0:
+    print(f"{x} is a negative number")
+elif x == 0:
+    print(f"{x} is zero")
+else:
+    print(f"{x} is positive")
+    
+    
+#for statements
+words = ['apple','banana','carrot','grapes','mango']
+for w in words:
+    print(w,len(w))
+
+
