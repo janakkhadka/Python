2b4978ed6d7d434f2c3ea45be2afcbf77032412d
janakkhadka <janakkhadka08@gmail.com>
2024-06-18 23:54:51 +0545

    function annotations

---
+++ Day8/funannot.py
@@ -0,0 +1,12 @@
+#4.8.8. Function Annotations
+
+def f(ham: str, eggs: str = 'eggs') -> str:
+
+    print("Annotations:", f.__annotations__)
+
+    print("Arguments:", ham, eggs)
+
+    return ham + ' and ' + eggs
+
+
+f('spam')
