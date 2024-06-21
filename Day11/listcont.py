2e02d7282f2629f67859d993c69d0f667a489ee5
janakkhadka <janakkhadka08@gmail.com>
2024-06-21 22:54:05 +0545

    list continue

---
+++ Day11/listcont.py
@@ -0,0 +1,15 @@
+#5.1.3. List Comprehensions
+
+squares = []
+
+for x in range(10):
+
+    squares.append(x**2)
+
+print(squares)
+
+squares = list(map(lambda x: x**2, range(10)))
+print(squares)
+
+squares = [x**2 for x in range(10)]
+print(squares)
