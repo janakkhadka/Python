9a29963e312ac1b96fa44f8f9fd1f86a77914f71
janakkhadka <janakkhadka08@gmail.com>
2024-06-27 23:02:39 +0545

    looping technique

--- Day16/loopingtech.py
+++ Day16/loopingtech.py
@@ -4,3 +4,10 @@ knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 for k, v in knights.items():
 
     print(k, v)
+    
+    
+    
+    
+for i, v in enumerate(['tic', 'tac', 'toe']):
+
+    print(i, v)
